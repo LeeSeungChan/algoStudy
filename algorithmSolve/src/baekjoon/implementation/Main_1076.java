430dbe72a4cc60c8d2da6961bd0cfb95cec5c973
LeeSeungChan <leeryan1814@gmail.com>
2020-11-29 20:36:51 +0900

    [2020-11-29] 백준온라인 implementation PASS
     - 1076
     - 1259
     - 3009

---
+++ algorithmSolve/src/baekjoon/implementation/Main_1076.java
@@ -0,0 +1,53 @@
+package baekjoon.implementation;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ * 구현
+ * 
+ * 1076번: 저항
+ * https://www.acmicpc.net/problem/1076
+ * 
+ * PASS
+ * 24053947
+ * 
+ */
+public class Main_1076 {
+	public static void main(String[] args) {
+		// input
+		Scanner sc = new Scanner(System.in);
+		String[] sVal = {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
+		String[] sArr = new String[3];
+		for(int i = 0; i < sArr.length; i++) {
+			sArr[i] = sc.nextLine();
+		}
+		StringBuilder sb = new StringBuilder();
+		
+		// logic
+		// 첫 번째, 두 번째 저항 옴
+		for(int i = 0; i < sArr.length - 1; i++) {
+			for(int j = 0; j < sVal.length; j++) {
+				if(sArr[i].equals(sVal[j])) {
+					sb.append(String.valueOf(j));
+				}
+			}
+		}
+		
+		// 세 번째 저항 곱셈
+		int m = 0;
+		for(int i = 0; i < sVal.length; i++) {
+			if(sVal[i].equals(sArr[2])) {
+				m = (int) Math.pow(10, i);
+			}
+		}
+		
+		BigInteger result = new BigInteger(sb.toString());
+		result = result.multiply(new BigInteger(String.valueOf(m)));
+		
+		// print
+		System.out.println(result);
+		
+		sc.close();
+	}
+}
---
+++ algorithmSolve/src/baekjoon/implementation/Main_1259.java
@@ -0,0 +1,49 @@
+package baekjoon.implementation;
+
+import java.util.Scanner;
+
+/**
+ * 분류
+ * 
+ * 1259번: 펠림드론수
+ * https://www.acmicpc.net/problem/1259
+ * 
+ * PASS
+ */
+public class Main_1259 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			// input & initialize
+			String str = String.valueOf(sc.nextInt());
+			int[] arr = new int[str.length()];
+			boolean flag = false;
+			
+			if(Integer.parseInt(str) == 0){
+				break;
+			}
+			
+			for(int i = 0; i < str.length(); i++) {
+				arr[i] = Integer.parseInt(String.valueOf(str.charAt(i)));
+			}
+			
+			// logic
+			for(int i = 0; i < arr.length / 2; i++) {
+				if(arr[i] != arr[arr.length - 1 - i]) {
+					flag = true;
+					break;
+				}
+			}
+			
+			// print
+			if(flag) {
+				System.out.println("no");
+			}else {
+				System.out.println("yes");
+			}
+		}
+		
+		sc.close();
+	}
+}
